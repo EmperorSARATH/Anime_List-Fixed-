5829fe0711ac834ff5fa9f0b1dcf4fd141f0b1af
EmperorSARATH <ukm7773@gmail.com>
2023-10-24 11:40:24 +0530

    MInor fix

---
+++ src/components/AnimeList.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react'
+import Card from '@mui/material/Card';
+import { CardMedia, Container, Typography } from '@mui/material';
+import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import db from "../Animelist/db.json";
+
+
+const AnimeList = ({filteredPersons,items,setItems}) => {
+
+
+const [loaded,setIsLoaded]=useState(false)
+// const[itemsList,setItemsList]=useState([])
+const [error, setError] = useState(null);
+console.log(filteredPersons);
+
+
+    useEffect(() => {
+
+        fetch("http://localhost:4000/api/getAll")
+    
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                    console.log(items);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    if (error) {
+        return <>{error.message}</>;
+    } else if (!loaded) {
+        return <>loading...</>;
+    } else {
+        const filtered = filteredPersons.map(person =>  <Card key={person.id} person={person} />); 
+   
+  return (
+        
+        <Container sx={{bgcolor:"#2367",width:"73%"}}>
+            
+          
+        {filteredPersons.map((item=>(
+             <Row style={{display:"inline-flex",marginRight:"20px",marginBottom:"10px"}}>
+            <Col xs={12} md={4} lg={3} >
+            <Card key={item} >
+                <Typography variant={"h6"}  gutterBottom >
+                {item.title}
+                </Typography>
+                <CardMedia  component="img"
+                            height="194"
+                            image={item.image}/>
+            </Card>
+            </Col>
+            </Row>
+        )))}
+         
+        </Container>
+  )
+}
+}
+
+export default AnimeList
